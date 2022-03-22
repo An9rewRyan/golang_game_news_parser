b18b1ff0e19c57d24c7d0db408b06c1869ccd509
Ерёменко Семён <deutschman1999@mail.ru>
2022-03-22 11:04:49 +0300

    fixed stopgame content paths

--- sites_structs/Sg_article.go
+++ sites_structs/Sg_article.go
@@ -8,7 +8,7 @@ func Get_sg_paths() root_structs.Article_paths {
 	PGD_paths := root_structs.Article_paths{
 		Links_xpath:            "//a[@class='article-image']/@href",
 		Title_xpath:            "//h1[@class = 'article-title']/text()",
-		Content_xpath:          "//section[@class='article']/descendant::text()[ancestor::p or ancestor::ul]",
+		Content_xpath:          "//text()[ancestor::section/@class='article']",
 		Pub_date_xpath:         "//script[@type='application/ld+json']",
 		Image_url_xpath:        "//section[@class='article']/descendant::img/@src | //div[@class='iframe_h']/data-iframe/@data-preroll-thumb",
 		Site_link:              "https://stopgame.ru/news/industry",
