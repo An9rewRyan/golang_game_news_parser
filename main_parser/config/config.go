9fda836763859cb29aca74de68dcaa1d5da51b48
Ерёменко Семён <deutschman1999@mail.ru>
2022-03-30 00:29:42 +0300

    ADD: js rendering now works via selenium on golang
    no to js!..

--- main_parser/config/config.go
+++ main_parser/config/config.go
@@ -1,5 +1,11 @@
 package config
 
+import (
+	"os"
+
+	"github.com/tebeka/selenium"
+)
+
 const MAX_amount_of_goroutines = 5
 
 //in millisecokonds
@@ -14,3 +20,14 @@ const Sleep_time = 10
 
 //
 var MAX_amount_of_loading_retries = 10 //not a constant beacause of use in error formatting
+
+const Selenium_path = "selenium/selenium-server.jar"
+const Gecko_driver_path = "selenium/geckodriver"
+const Chrome_driver_path = "selenium/chromedriver"
+const Selenium_port = 8080
+
+var Selenium_opts = []selenium.ServiceOption{
+	selenium.StartFrameBuffer(),             // Start an X frame buffer for the browser to run in.
+	selenium.GeckoDriver(Gecko_driver_path), // Specify the path to GeckoDriver in order to use Firefox.
+	selenium.Output(os.Stderr),
+}
